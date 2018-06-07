95f4531b3d79144dc40b1a0fd67bd32f585b75df
bhjnmk <32776384+bhjnmk@users.noreply.github.com>
2018-06-07 17:29:50 +0200

    update

--- VisionSystem/Form1.Designer.cs
+++ VisionSystem/Form1.Designer.cs
@@ -35,16 +35,32 @@
             this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.capture_btn = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(25, 31);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(439, 452);
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -54,7 +70,7 @@
             this.cameraToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(952, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(903, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -83,9 +99,9 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(486, 31);
+            this.pictureBox2.Location = new System.Drawing.Point(236, 31);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(439, 452);
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
@@ -97,11 +113,106 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(445, 31);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(652, 31);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(118, 3);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(75, 23);
+            this.start_btn.TabIndex = 7;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(316, 367);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(469, 367);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(615, 367);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 10;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(25, 237);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(236, 237);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(174, 111);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // capture_btn
+            // 
+            this.capture_btn.Location = new System.Drawing.Point(456, 250);
+            this.capture_btn.Name = "capture_btn";
+            this.capture_btn.Size = new System.Drawing.Size(75, 23);
+            this.capture_btn.TabIndex = 7;
+            this.capture_btn.Text = "Capture";
+            this.capture_btn.UseVisualStyleBackColor = true;
+            this.capture_btn.Click += new System.EventHandler(this.capture_btn_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(634, 237);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(174, 111);
+            this.richTextBox2.TabIndex = 13;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(952, 509);
+            this.ClientSize = new System.Drawing.Size(903, 509);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.capture_btn);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -113,6 +224,12 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +244,16 @@
         private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button capture_btn;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- VisionSystem/Form1.cs
+++ VisionSystem/Form1.cs
@@ -7,8 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using Emgu.CV;
 using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
 
 namespace VisionSystem
 {
@@ -16,12 +19,32 @@ namespace VisionSystem
     {
         VideoCapture cameraCapture;
         Image<Bgr, Byte> currentFrame;
-        Image<Hsv, Byte> hsv = null;
         Image<Ycc, Byte> ycc = null;
+        Image<Ycc, Byte> findColor = null;
+        Image<Gray, Byte> binary = null;
+        Image<Gray, Byte> blurBinary = null;
+        Image<Gray, byte> output;
+        int largest_contour_index = 0;
+        double largest_area = 0;
+
+        int x = 1;
+        int y = 1;
+        int number = 3;
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            if (cameraCapture == null)
+            {
+                cameraCapture = new VideoCapture(0);
+                cameraCapture.QueryFrame();
+
+                Application.Idle += Capture_ImageGrabbed;
+            }
         }
 
         private void startToolStripMenuItem_Click(object sender, EventArgs e)
@@ -33,20 +56,53 @@ namespace VisionSystem
                 
                 Application.Idle += Capture_ImageGrabbed;
             }
-            //cameraCapture.ImageGrabbed += Capture_ImageGrabbed;
-            //cameraCapture.Start();
         }
 
         private void Capture_ImageGrabbed(object sender, EventArgs e)
         {
             try
             {
-                currentFrame = cameraCapture.QueryFrame().ToImage<Bgr, Byte>();
+                currentFrame = cameraCapture.QueryFrame().ToImage<Bgr, Byte>().Resize(0.4, Emgu.CV.CvEnum.Inter.Cubic);
                 ycc = currentFrame.Convert<Ycc, Byte>();
-                
-                pictureBox1.Image = currentFrame.Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
-                pictureBox2.Image = FindColor(ycc).Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
+                findColor = FindColor(ycc);
+                binary = ChangeToBinary(findColor);
+                blurBinary = binary.SmoothMedian(5);
+                output = new Image<Gray, byte>(blurBinary.Width, blurBinary.Height, new Gray(0));
+
+                pictureBox1.Image = blurBinary.Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
+
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat hierarchy = new Mat();
+                CvInvoke.FindContours(
+                    blurBinary,
+                    contours,
+                    hierarchy,
+                    RetrType.External,
+                    ChainApproxMethod.ChainApproxSimple
+                    );
+
+                    for (int i = 0; i < contours.Size; i++)
+                    {
+                        double a = CvInvoke.ContourArea(contours[i], false);
+                    //if (a > largest_area)
+                    //{
+                    //    largest_area = a;
+                    //    largest_contour_index = i;
+                    //}
+                    CvInvoke.DrawContours(output, contours, i, new MCvScalar(255, 0, 0), 2);
+                }
+
+                    //CvInvoke.DrawContours(output, contours, largest_contour_index, new MCvScalar(255, 0, 0), 2);
+
+                    richTextBox1.Text = richTextBox1.Text + contours.Size + Environment.NewLine;
+                //pictureBox1.Image = currentFrame.Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
+                //pictureBox2.Image = findColor.Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
+                //pictureBox3.Image = binary.Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
+                pictureBox2.Image = output.Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
+
+
                 textBox1.Text = currentFrame.Bitmap.Size.Width.ToString();
+                
             }
             catch (Exception)
             {
@@ -54,21 +110,40 @@ namespace VisionSystem
             }
         }
 
-        public Image<Gray, Byte> FindColor(Image<Ycc, Byte> yccimage)
+        public Image<Ycc, Byte> FindColor(Image<Ycc, Byte> yccimage)
         {
             Image<Ycc, Byte> ret = yccimage;
             var image = yccimage.InRange(new Ycc(30, 135, 85), new Ycc(235, 240, 240));
             var mat = yccimage.Mat;
             mat.SetTo(new MCvScalar(0), image);
-            Image<Gray,Byte> gray = mat.ToImage<Gray, Byte>();
-            gray = gray.ThresholdBinary(new Gray(50), new Gray(255)).Not();
-            gray = gray.Dilate(6);
-
             mat.CopyTo(ret);
+
+            return ret;
+        }
+
+        public Image<Gray, Byte> ChangeToBinary(Image<Ycc, Byte> fcimage)
+        {
+            var mat = fcimage.Mat;
+            Image<Gray, Byte> gray = mat.ToImage<Gray, Byte>();
+            gray = gray.ThresholdBinary(new Gray(50), new Gray(255)).Not();
             return gray;
         }
-        
 
+        //public IOutputArray FindContours(Image<Gray,Byte> binaryImage)
+        //{
+        //    VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+        //    Mat hierarchy = new Mat();
+        //    CvInvoke.FindContours(
+        //        binaryImage,
+        //        contours,
+        //        hierarchy,
+        //        RetrType.Ccomp,
+        //        ChainApproxMethod.ChainApproxSimple
+        //        );
+        
+        //    return contours;
+        //}
+        
         private void stopToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (cameraCapture != null)
@@ -80,11 +155,38 @@ namespace VisionSystem
         private void pictureBox2_Paint(object sender, PaintEventArgs e)
         {
            
-                Rectangle ee = new Rectangle(1, 1, Convert.ToInt32(pictureBox2.Width / 1.5), Convert.ToInt32(pictureBox2.Height / 1.5));
-                using (Pen pen = new Pen(Color.Red, 1))
-                {
-                    e.Graphics.DrawRectangle(pen, ee);
-                }
+        //        Rectangle ee = new Rectangle(1, 1, Convert.ToInt32(pictureBox2.Width / 1.5), Convert.ToInt32(pictureBox2.Height / 1.5));
+        //        using (Pen pen = new Pen(Color.Red, 1))
+        //        {
+        //            e.Graphics.DrawRectangle(pen, ee);
+        //        }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            x = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            y = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            number = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void capture_btn_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Image = output.Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
+            
+            richTextBox2.Text = richTextBox2.Text + output.GetType();
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
