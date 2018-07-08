d214cc2d5d368bbdfbba5d87038bb5d0dd654374
Radek Zajkowski <zajkowskiradek@gmail.com>
2018-07-09 01:02:19 +0200

    [update] new class

--- VisionSystem/Form1.Designer.cs
+++ VisionSystem/Form1.Designer.cs
@@ -36,32 +36,29 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.start_btn = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.linearSpeed = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.capture_btn = new System.Windows.Forms.Button();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.linearSpeedLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.linearSpeed)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(25, 31);
+            this.pictureBox1.Location = new System.Drawing.Point(687, 48);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -71,7 +68,7 @@
             this.cameraToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(903, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1064, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -87,25 +84,24 @@
             // startToolStripMenuItem
             // 
             this.startToolStripMenuItem.Name = "startToolStripMenuItem";
-            this.startToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.startToolStripMenuItem.Text = "IP Camera";
             this.startToolStripMenuItem.Click += new System.EventHandler(this.IPCamera_Click);
             // 
             // stopToolStripMenuItem
             // 
             this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
-            this.stopToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.stopToolStripMenuItem.Text = "Embeded Camera";
             this.stopToolStripMenuItem.Click += new System.EventHandler(this.EmbededCamera_Click);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(236, 31);
+            this.pictureBox2.Location = new System.Drawing.Point(778, 48);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
             // 
             // textBox1
             // 
@@ -116,19 +112,12 @@
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(445, 31);
+            this.pictureBox3.Location = new System.Drawing.Point(25, 48);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox3.Size = new System.Drawing.Size(600, 600);
             this.pictureBox3.TabIndex = 4;
             this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Location = new System.Drawing.Point(652, 31);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox4.TabIndex = 6;
-            this.pictureBox4.TabStop = false;
+            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
             // 
             // start_btn
             // 
@@ -140,64 +129,55 @@
             this.start_btn.UseVisualStyleBackColor = true;
             this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
             // 
-            // numericUpDown1
+            // linearSpeed
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(316, 367);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 8;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.linearSpeed.Location = new System.Drawing.Point(640, 142);
+            this.linearSpeed.Name = "linearSpeed";
+            this.linearSpeed.Size = new System.Drawing.Size(120, 20);
+            this.linearSpeed.TabIndex = 8;
+            this.linearSpeed.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.linearSpeed.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(469, 367);
+            this.numericUpDown2.Location = new System.Drawing.Point(781, 142);
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
             this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
             // numericUpDown3
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(615, 367);
+            this.numericUpDown3.Location = new System.Drawing.Point(920, 142);
             this.numericUpDown3.Name = "numericUpDown3";
             this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown3.TabIndex = 10;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
             this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Location = new System.Drawing.Point(25, 237);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox5.TabIndex = 11;
-            this.pictureBox5.TabStop = false;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(236, 237);
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.richTextBox1.Location = new System.Drawing.Point(643, 626);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(175, 36);
+            this.richTextBox1.Size = new System.Drawing.Size(117, 22);
             this.richTextBox1.TabIndex = 12;
             this.richTextBox1.Text = "";
-            // 
-            // capture_btn
-            // 
-            this.capture_btn.Location = new System.Drawing.Point(456, 250);
-            this.capture_btn.Name = "capture_btn";
-            this.capture_btn.Size = new System.Drawing.Size(75, 23);
-            this.capture_btn.TabIndex = 7;
-            this.capture_btn.Text = "Capture";
-            this.capture_btn.UseVisualStyleBackColor = true;
-            this.capture_btn.Click += new System.EventHandler(this.capture_btn_Click);
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(236, 279);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(175, 36);
-            this.richTextBox2.TabIndex = 13;
-            this.richTextBox2.Text = "";
-            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
             // 
             // button1
             // 
@@ -209,21 +189,49 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.stop_btn_Click);
             // 
+            // linearSpeedLabel
+            // 
+            this.linearSpeedLabel.AutoSize = true;
+            this.linearSpeedLabel.Location = new System.Drawing.Point(665, 126);
+            this.linearSpeedLabel.Name = "linearSpeedLabel";
+            this.linearSpeedLabel.Size = new System.Drawing.Size(70, 13);
+            this.linearSpeedLabel.TabIndex = 13;
+            this.linearSpeedLabel.Text = "Linear Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(798, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Joint Speed";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.richTextBox2.Location = new System.Drawing.Point(766, 206);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(274, 442);
+            this.richTextBox2.TabIndex = 15;
+            this.richTextBox2.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(903, 509);
+            this.ClientSize = new System.Drawing.Size(1064, 681);
             this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linearSpeedLabel);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.numericUpDown3);
             this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.capture_btn);
+            this.Controls.Add(this.linearSpeed);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.start_btn);
-            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox2);
@@ -238,11 +246,9 @@
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.linearSpeed)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -258,16 +264,15 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.Button start_btn;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown linearSpeed;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.NumericUpDown numericUpDown3;
-        private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button capture_btn;
-        private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label linearSpeedLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- VisionSystem/Form1.cs
+++ VisionSystem/Form1.cs
@@ -13,6 +13,9 @@ using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using Emgu.CV.CvEnum;
 using AForge.Video;
+using System.Drawing.Drawing2D;
+using System.Collections;
+using System.ComponentModel;
 
 namespace VisionSystem
 {
@@ -31,14 +34,13 @@ namespace VisionSystem
 
         MJPEGStream stream;
         string camera = "";
-        int x = 1;
-        int y = 1;
+        int xLinear = 1;
+        int yLinear = 1;
         int number = 3;
         string stop = "stop";
         string run = "run";
-        int z = 0;
-        int z2 = 0;
-        int z3 = 0;
+        private PaintEventArgs ee;
+
         public Form1()
         {
             InitializeComponent();
@@ -59,14 +61,14 @@ namespace VisionSystem
             else if (camera == "ip")
             {
 
-                stream = new MJPEGStream("http://192.168.0.80:4747/video");
-                stream.NewFrame += stream_NewFrame;
-                stream.Start();
+                //stream = new MJPEGStream("http://192.168.0.80:4747/video");
+                //stream.NewFrame += stream_NewFrame;
+                //stream.Start();
 
             }
         }
 
-        private void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        private void stream_NewFrame(object sender, NewFrameEventArgs eventArgs, PaintEventArgs ee)
         {
             Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
             pictureBox1.Image = bmp;
@@ -78,7 +80,7 @@ namespace VisionSystem
         {
             try
             {
-                currentFrame = cameraCapture.QueryFrame().ToImage<Bgr, Byte>().Resize(0.4, Emgu.CV.CvEnum.Inter.Cubic);
+                currentFrame = cameraCapture.QueryFrame().ToImage<Bgr, Byte>().Resize(0.9, Emgu.CV.CvEnum.Inter.Cubic);
                 ycc = currentFrame.Convert<Ycc, Byte>();
                 // findColor = FindColor(ycc);
                 //binary = ChangeToBinary(findColor);
@@ -87,65 +89,90 @@ namespace VisionSystem
                 pictureBox1.Image = ycc.ToBitmap();
                 Image<Gray, Byte> gray = ycc.Convert<Gray, Byte>(); //Convert it to Grayscale
                 pictureBox2.Image = gray.ToBitmap();
-
                 CascadeClassifier fist = new CascadeClassifier("C:/Emgu/emgucv-windesktop 3.4.1.2976/etc/haarcascades/fist.xml");
                 CascadeClassifier palm = new CascadeClassifier("C:/Emgu/emgucv-windesktop 3.4.1.2976/etc/haarcascades/palm.xml");
-                
-                
-                 CascadeClassifier okey = new CascadeClassifier("C:/Users/Zajkos/source/VisionSystem/VisionSystem/classifier/cascade-okey2.xml");
+
+               // CascadeClassifier rock = new CascadeClassifier("C:/Users/Zajkos/source/VisionSystem/VisionSystem/classifier/gesture-rock4.xml");
+                //CascadeClassifier okey = new CascadeClassifier("C:/OpenCV/opencv/build/x64/vc14/bin/test gesture/elephant_classifier.xml");
+
+                CascadeClassifier okey = new CascadeClassifier(" C:/Users/Zajkos/Desktop/gest rock/.xml");
+               
+               // CascadeClassifier victoria = new CascadeClassifier("C:/Users/Zajkos/source/VisionSystem/VisionSystem/classifier/gesture-victoria.xml");
 
                 Rectangle[] rectangles;
                 Rectangle[] rectangles2;
-                //Rectangle[] rectangles3;
+                Rectangle[] rectangles3;
                 Rectangle[] rectangles4;
+                Rectangle[] rectangles5;
 
                 // detect eyes.
                 rectangles = fist.DetectMultiScale(gray, scaleFactor: 1.2, minNeighbors: 12);
                 rectangles2 = palm.DetectMultiScale(gray, scaleFactor: 1.2, minNeighbors: 12);
-                //rectangles3 = okey.DetectMultiScale(gray, scaleFactor: 1.2, minNeighbors: 12);
+               //rectangles3 = rock.DetectMultiScale(gray, scaleFactor: 1.2, minNeighbors: 12);
                 rectangles4 = okey.DetectMultiScale(gray, scaleFactor: 1.2, minNeighbors: 12);
+                //rectangles5 = victoria.DetectMultiScale(gray, scaleFactor: 1.2, minNeighbors: 12);
 
 
                 foreach (var rectangle in rectangles)
                     {
-                    // draw detected locations.
-                    z = z + 1; //File.Create("C:/Emgu/test/my" + z + ".bmp");
                     ycc.Draw(rectangle, new Ycc(122,222,12));
                     richTextBox1.Text = String.Empty + "run";
-                    // ycc.Save("test.jpg");
+                    //richTextBox1.Text = richTextBox1.Text + xLinear.ToString();
                     
-                  // ycc.Save("C:/Emgu/test/my" + z + ".bmp"); 
 
-    }
+                }
                 foreach (var rectangle2 in rectangles2)
                 {
-                    // draw detected locations.
+                    if (rectangle2.Height >= 80)
+                    {
+                        ycc.Draw(rectangle2, new Ycc(1, 2, 255));
+                        // Create string to draw.
+                        String drawString = "sad";
 
-                    ycc.Draw(rectangle2, new Ycc(1, 2, 255));
-                    richTextBox1.Text = String.Empty + "stop";
+                        // Create font and brush.
+                        Font drawFont = new Font("Arial", 16);
+                        SolidBrush drawBrush = new SolidBrush(Color.Black);
 
-                    
+                        // Create point for upper-left corner of drawing.
+                        PointF drawPoint = new PointF(150.0F, 150.0F);
+
+                        // Draw string to screen.
+                        ee.Graphics.DrawString(drawString, drawFont, drawBrush, drawPoint);
+                        richTextBox1.Text = String.Empty + "stop";
+                    }
                 }
-/*
-                foreach (var rectangle3 in rectangles3)
-                {
-                    // draw detected locations.
 
-                    ycc.Draw(rectangle3, new Ycc(185, 148, 185));
-                    richTextBox1.Text = String.Empty + "okey";
+                //foreach (var rectangle3 in rectangles3)
+                //{
+                   
+                //    if (rectangle3.Height >= 80)
+                //    {
+                //        ycc.Draw(rectangle3, new Ycc(185, 148, 185));
+                //        richTextBox1.Text = String.Empty + "rock";
+                //        richTextBox2.Text = richTextBox2.Text + rectangle3;
+                //    }
+                      
+                //}
+                
 
-
-                }*/
                 foreach (var rectangle4 in rectangles4)
-                {
-                    // draw detected locations.
-
-                    ycc.Draw(rectangle4, new Ycc(18, 12, 15));
-                    richTextBox1.Text = String.Empty + "ok";
-
-
+                    {
+                    if (rectangle4.Height >= 100)
+                    {
+                        ycc.Draw(rectangle4, new Ycc(18, 12, 15));
+                        richTextBox1.Text = String.Empty + "ok";
+                               //richTextBox2.Text = richTextBox2.Text + rectangle4;
+                    }
                 }
+                //foreach (var rectangle5 in rectangles5)
+                //{
+                //    // draw detected locations.
 
+                //    ycc.Draw(rectangle5, new Ycc(138, 210, 138));
+                //    richTextBox1.Text = String.Empty + "victoria";
+
+
+                //}
 
                 pictureBox3.Image = ycc.ToBitmap();
                 
@@ -155,6 +182,8 @@ namespace VisionSystem
 
             }
         }
+
+       
 
         public Image<Ycc, Byte> FindColor(Image<Ycc, Byte> yccimage)
         {
@@ -198,55 +227,56 @@ namespace VisionSystem
             }
         }
 
-        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+       
+        private void pictureBox3_Paint(object sender, PaintEventArgs e)
         {
-           
-        //        Rectangle ee = new Rectangle(1, 1, Convert.ToInt32(pictureBox2.Width / 1.5), Convert.ToInt32(pictureBox2.Height / 1.5));
-        //        using (Pen pen = new Pen(Color.Red, 1))
-        //        {
-        //            e.Graphics.DrawRectangle(pen, ee);
-        //        }
+            Rectangle yp = new Rectangle(225, 1, Convert.ToInt32(pictureBox3.Width / 6), Convert.ToInt32(pictureBox3.Height / 6));
+            Rectangle ym = new Rectangle(225, 319, Convert.ToInt32(pictureBox3.Width / 6), Convert.ToInt32(pictureBox3.Height / 6));
+            Rectangle xp = new Rectangle(1, 150, Convert.ToInt32(pictureBox3.Width / 6), Convert.ToInt32(pictureBox3.Height / 6));
+            Rectangle xm = new Rectangle(460, 150, Convert.ToInt32(pictureBox3.Width / 6), Convert.ToInt32(pictureBox3.Height / 6));
+            Rectangle zp = new Rectangle(460, 1, Convert.ToInt32(pictureBox3.Width / 6), Convert.ToInt32(pictureBox3.Height / 6));
+            Rectangle zm = new Rectangle(1,319, Convert.ToInt32(pictureBox3.Width / 6), Convert.ToInt32(pictureBox3.Height / 6));
+
+            Rectangle menu = new Rectangle(225, 150, Convert.ToInt32(pictureBox3.Width / 6), Convert.ToInt32(pictureBox3.Height / 6));
+            using (Pen pen = new Pen(Color.Blue, 1))
+            {
+                e.Graphics.DrawRectangle(pen, yp);
+                e.Graphics.DrawRectangle(pen, ym);
+            }
+            using (Pen pen = new Pen(Color.Red, 1))
+            {
+                e.Graphics.DrawRectangle(pen, xp);
+                e.Graphics.DrawRectangle(pen, xm);
+            }
+            using (Pen pen = new Pen(Color.Green, 1))
+            {
+                e.Graphics.DrawRectangle(pen, zp);
+                e.Graphics.DrawRectangle(pen, zm);
+            }
+            using (Pen pen = new Pen(Color.Orange, 1))
+            {
+                e.Graphics.DrawRectangle(pen, menu);
+               
+            }
+
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            x = Convert.ToInt32(numericUpDown1.Value);
+            xLinear = Convert.ToInt32(linearSpeed.Value);
         }
 
         private void numericUpDown2_ValueChanged(object sender, EventArgs e)
         {
-            y = Convert.ToInt32(numericUpDown1.Value);
+            yLinear = Convert.ToInt32(linearSpeed.Value);
         }
 
         private void numericUpDown3_ValueChanged(object sender, EventArgs e)
         {
-            number = Convert.ToInt32(numericUpDown1.Value);
+            number = Convert.ToInt32(linearSpeed.Value);
         }
 
-        private void capture_btn_Click(object sender, EventArgs e)
-        {
-            if (camera == "embeded")
-            {
-                pictureBox5.Image = pictureBox1.Image;
-                z = z + 1;
-                ycc.Save("C:/Emgu/test/haar_classifier--okey" + z + ".jpg");
-
-                if (cameraCapture == null)
-                {
-                    cameraCapture = new VideoCapture(0);
-                    cameraCapture.QueryFrame();
-                    Application.Idle += Capture_ImageGrabbed;
-                }
-            }
-            else if (camera == "ip")
-            {
-
-                pictureBox5.Image = pictureBox1.Image;
-
-            }
-            //pictureBox3.Image = output.Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap;
-            //richTextBox2.Text = richTextBox2.Text + output.GetType();
-        }
+       
 
         private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
